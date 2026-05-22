09c9087f8ba86e3b49b4109aa960fc7388402eb3
Alison <alisongustavovalli@gmail.com>
2026-05-22 03:23:23 -0300

    Adição da pasta 'includes', com estrutura e ideia do header e footer desenvolvida.

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+</main>
+<footer>
+    <p>Desenvolvido por:</p>
+    <div class="dfooter">
+        <p>Alison Gustavo Valli</p>
+        <p>Julia Furtado Polycarpo</p>
+    </div>
+    <p>2ºC¹ - 2026</p>
+</footer>   
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FarmAí</title>
+    <link rel="icon" type="image/png" href="imagens/icone.png">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="dlogo"><img src="imagens/logo.png" class="logo"></div>
+            <input type="checkbox" id="menu-toggle">
+            <label for="menu-toggle" class="menu-icon">☰</label>
+            <ul class="menu">
+                <li><a href="index.php">Início</a></li>
+                <li><a href="cadastro.php">Cadastro</a></li>
+                <li><a href="visualizar.php">Visualizar</a></li>
+            </ul>
+        </nav>
+    </header>
+<main>
